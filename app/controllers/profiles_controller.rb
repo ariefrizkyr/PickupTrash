b8578caf6365d3b9f10c99ed92a54f3137a408e4
Arief R Ramadhan <ariefrizkyr@gmail.com>
2016-06-16 08:34:47 +0700

    Added update profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,25 +1,54 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_user
+  before_action :find_profile, only: [:edit, :update]
+  before_action :only_current_user
+
   def new
-    @user = User.find(params[:user_id])
     @profile = Profile.new
   end
 
   def create
-    @user = User.find(params[:user_id])
     @profile = @user.build_profile(profile_params)
     if @profile.save
-      flash[:success] = 'Profile Updated!'
+      flash[:success] = 'Profile Created!'
       redirect_to user_path(params[:user_id])
     else
+      flash[:error] = 'Error occured!'
       render 'new'
     end
   end
 
   def edit
-    
+
+  end
+
+  def update
+    if @profile.update_attributes(profile_params)
+      flash[:success] = 'Resume Updated!'
+      redirect_to user_path(params[:user_id])
+    else
+      flash[:error] = 'Error occured!'
+      render 'edit'
+    end
   end
 
   private
+    def find_user
+      @user = User.find(params[:user_id])
+    end
+
+    def find_profile
+      @profile = @user.profile
+    end
+
+    def only_current_user
+      unless @profile.user_id == current_user.id
+        flash[:error] = 'Access denied'
+        redirect_to root_path
+      end
+    end
+
     def profile_params
       params.require(:profile).permit(:address, :city, :province, :phone_number,
                                       :bank_name, :account_number, :account_name)
