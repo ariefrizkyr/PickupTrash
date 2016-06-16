c56fcd12eb2d6ab7302fe286bc5e83f2ac061b93
Arief R Ramadhan <ariefrizkyr@gmail.com>
2016-06-16 08:01:34 +0700

    Added create profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,27 @@
 class ProfilesController < ApplicationController
   def new
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
     @profile = @user.build_profile
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = 'Profile Updated!'
+      redirect_to user_path(params[:user_id])
+    else
+      render 'new'
+    end
   end
 
   def edit
 
   end
+
+  private
+    def profile_params
+      params.require(:profile).permit(:address, :city, :province, :phone_number,
+                                      :bank_name, :account_number, :account_name)
+    end
 end
