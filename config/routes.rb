693ab764a56732cbe686851a4336d7659b9a25db
Arief R Ramadhan <ariefrizkyr@gmail.com>
2016-06-16 08:58:29 +0700

    Added admin role

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   devise_for :users
+  devise_for :admins
 
   resources :users do
     resource :profile
   end
-  
+
   get '/pricing' => 'pages#pricing'
   get '/termofuse' => 'pages#term'
 end
